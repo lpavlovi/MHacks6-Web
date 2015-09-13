752a9c64f896a822bd7b5e1f906f080636836860
Peter Chu <cgpchu@uwaterloo.ca>
2015-09-12 22:03:41 -0400

    add mongodb

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ var app = express();
 var port_number = process.env.PORT;
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var mongodb = require('mongodb');
+var mongo_client = mongodb.MongoClient;
+var url = 'mongodb://localhost:27017/task_database';
+mongo_client.connect(url, function (err, db) {
+    if (err) {
+        console.log('Unable to connect to the mongoDB server. Error:', err);
+    } else {
+        // Connection successful
+        db.close();
+    }
+});
 
 app.use('/', express.static(__dirname + '/project'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
